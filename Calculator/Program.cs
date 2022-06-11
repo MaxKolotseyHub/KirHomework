314fc2540bb24186e290e25e2102bc62b35d88d7
Max <kolotseymax@gmail.com>
2022-06-11 12:36:42 +0300

    первая домашка про калькулятор

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,6 +4,23 @@ namespace Calculator
 {
     class Program
     {
+        //Напиши калькулятор.
+        //это должно быть консольное приложение,
+        //которое просит тебя ввести два числа и потом просит тебя сказать,
+        //что с ними сделать
+
+        //должно быть четыре метода:
+        //    sum(int i, int j); - сумма
+        //    sub(int i, int j); - вычитание
+        //    mul(int i, int j); - умножение
+        //    div(int i, int j); - деление
+
+        //может понадобиться:
+        //int a = int.Parse(Console.ReadLine());
+        //switch
+        //if - else
+
+        //не забудь как-нибудь обработать условие, что на ноль делить нельзя, я проверю!
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
